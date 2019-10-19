b06a2ba4aaf0c4688668839bca26fa0acf2ca19e
itscap <encappozzo@gmail.com>
2019-10-19 14:51:41 +0200

    Fix open forlder if running windows

--- src/sh_manager.js
+++ src/sh_manager.js
@@ -1,12 +1,19 @@
 "use strict";
 const proc = require("child_process");
 const path = require("path");
+const isWin = process.platform === "win32";
 
 async function openFolder(filePath) {
-    let cmd = "open file:"
-        + path.sep
-        + path.sep
-        + filePath
+    let cmd = null
+    if (isWin) {
+        cmd = "start "
+            + filePath
+    } else {
+        cmd = "open file:"
+            + path.sep
+            + path.sep
+            + filePath
+    }
 
     try {
         await _sh(cmd)
